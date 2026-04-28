97e2b5c15fdde807315a25db1746263770693355
Artur Drzazga <artur.drzazga03@gmail.com>
2026-04-28 16:50:57 +0200

    Add borrowings filtering

--- borrowings/views.py
+++ borrowings/views.py
@@ -12,3 +12,23 @@ class BorrowingsViewSet(viewsets.ModelViewSet):
         if self.action == "create":
             return BorrowingCreateSerializer
         return BorrowingSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        q = self.queryset
+
+        if not user.is_staff:
+            q = q.filter(user_id=user.id)
+
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+
+        if is_active == "true":
+            q = q.filter(actual_return_date__isnull=True)
+        elif is_active == "false":
+            q = q.filter(actual_return_date__isnull=False)
+
+        if user_id:
+            q = q.filter(user_id=user_id)
+
+        return q
