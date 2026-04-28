3a4b7803640c48fbe337fdbac4f37650382281e9
Artur Drzazga <artur.drzazga03@gmail.com>
2026-04-28 16:52:52 +0200

    Add borrowings return endpoint

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,11 +1,15 @@
+from datetime import date
+
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 from borrowings.models import Borrowing
 from borrowings.serializers import BorrowingSerializer, BorrowingCreateSerializer
 
 
 class BorrowingsViewSet(viewsets.ModelViewSet):
-    queryset = Borrowing.objects.select_related("book", "user")
     serializer_class = BorrowingSerializer
 
     def get_serializer_class(self):
@@ -15,7 +19,7 @@ class BorrowingsViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        q = self.queryset
+        q = Borrowing.objects.select_related("book", "user")
 
         if not user.is_staff:
             q = q.filter(user_id=user.id)
@@ -32,3 +36,17 @@ class BorrowingsViewSet(viewsets.ModelViewSet):
             q = q.filter(user_id=user_id)
 
         return q
+
+    @action(detail=True, methods=["post"], url_path="return")
+    def return_book(self, request, pk=None):
+        borrowing = self.get_object()
+
+        if borrowing.actual_return_date:
+            raise ValidationError("Borrowing already returned")
+
+        borrowing.actual_return_date = date.today()
+        borrowing.save()
+        borrowing.book.inventory += 1
+        borrowing.book.save()
+
+        return Response(BorrowingSerializer(borrowing).data)
