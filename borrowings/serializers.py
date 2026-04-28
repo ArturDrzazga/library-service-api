742ebaaea8257a01574b0c9b29df2001bc182811
Artur Drzazga <artur.drzazga03@gmail.com>
2026-04-28 16:49:50 +0200

    Add borrowings create endpoint

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from rest_framework import serializers
 
 from books.serializers import BookSerializer
@@ -18,3 +20,31 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "book",
             "user",
         ]
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Borrowing
+        fields = [
+            "id",
+            "book",
+            "expected_return_date",
+        ]
+
+    def validate_book(self, book):
+        if book.inventory == 0:
+            raise serializers.ValidationError("Book is not available")
+        return book
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        book = validated_data["book"]
+        book.inventory -= 1
+        book.save()
+
+        return Borrowing.objects.create(
+            user=user,
+            borrow_date=date.today(),
+            **validated_data
+        )
--- borrowings/views.py
+++ borrowings/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer
+from borrowings.serializers import BorrowingSerializer, BorrowingCreateSerializer
 
 
-class BorrowingsViewSet(viewsets.ReadOnlyModelViewSet):
+class BorrowingsViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.select_related("book", "user")
     serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingSerializer
