3f74f170649d456def79f16c4e4938826ff8c868
Artur Drzazga <artur.drzazga03@gmail.com>
2026-04-28 16:41:21 +0200

    Add borrowing model

--- LibraryService/settings.py
+++ LibraryService/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "books",
     "users",
+    "borrowings",
 ]
 
 AUTH_USER_MODEL = "users.User"
--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
